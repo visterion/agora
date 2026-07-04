02318fe913493161289f88f6df160d1b94d7e2fd
vivu <vivu@users.noreply.github.com>
2026-07-04 03:01:19 +0200

    feat(data): optional FxWarmer (static pair list, layer-safe, default off)

--- src/main/java/de/visterion/agora/AgoraApplication.java
+++ src/main/java/de/visterion/agora/AgoraApplication.java
@@ -2,8 +2,10 @@ package de.visterion.agora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AgoraApplication {
     public static void main(String[] args) {
         SpringApplication.run(AgoraApplication.class, args);
